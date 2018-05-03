7ea7cd53652f1d42c5450672a898e080e789add8
jiangbo <1062417355@qq.com>
2018-05-03 17:24:43 +0800

    增加订单列表页面，常见问题页面

--- pages/orderList/orderList.js
+++ pages/orderList/orderList.js
@@ -5,7 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      orderList: [
+          {
+              type: '外卖',
+              list: [{
+                  name: '米饭'
+              }],
+              createdTime: '2018-02-11 11:39',
+              total: '24.9'
+          }
+      ]
   },
 
   /**
